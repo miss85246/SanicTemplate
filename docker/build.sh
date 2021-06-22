a874332a4e8678bb166f4d5d3971e6beee2a62cc
Connor_Zhang <63054779@qq.com>
2021-06-23 02:26:52 +0800

    优化 Dockerfile 和 build.sh 文件， 减少 docker 镜像的层级

--- docker/build.sh
+++ docker/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-cd ../..
+cd ..
 
 TIME_NOW=$(date +%Y%m%d)
 UUID=$(uuidgen)
 UUID=${UUID##*-}
 SUFFIX="${TIME_NOW}_${UUID}"
 
-docker build -f docker/Dockerfile -t dockerhub.datagrand.com/databj/k5/customize_api:"${SUFFIX}" .
-docker push dockerhub.datagrand.com/databj/k5/customize_api:"${SUFFIX}"
+docker build -f docker/Dockerfile -t dockerhub.datagrand.com/databj/zhangyue/sanic_template:"${SUFFIX}" .
+#docker push dockerhub.datagrand.com/databj/zhangyue/sanicTemplate:"${SUFFIX}"
