ceda26a8ea56146dc2e6e99b5e87742ff8546b8c
zhangyue <zhangyue@datagrand.com>
2021-09-02 17:18:08 +0800

    清除 master 的内容

--- deploy/restart.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sh ./shutdown.sh && sh ./start.sh
--- test/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-"""
-FileName: __init__.py
-Description: 
-Author: ConnorZhang
-Email: zhangyue@datagrand.com
-CreateTime: 2021-05-24
-"""
